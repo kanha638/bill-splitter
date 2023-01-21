1322da6f9944d3a09dae551aa928c764855d24e2
kamal kishor <ikkamalkihs0r@gmail.com>
2023-01-21 19:05:38 +0530

    initialised frontend

---
+++ client/src/Components/Group.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Group = () => {
+  return (
+    <>
+        <div className="group-container">
+          // group admin can add members
+          // it can have a members tab
+          // anyone in group can create an entry 
+          // each group will have list of entries
+        </div>
+    </>
+  )
+}
+
+export default Group
